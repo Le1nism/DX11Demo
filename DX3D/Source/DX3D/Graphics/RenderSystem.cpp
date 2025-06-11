6fd5d5f4e6d0c4c7c0882decbf9718c76f96ef50
le1nism <cristian.capiferri@gmail.com>
2025-06-11 21:19:29 +0200

    Initialized D3D11

--- DX3D/Source/DX3D/Graphics/RenderSystem.cpp
+++ DX3D/Source/DX3D/Graphics/RenderSystem.cpp
@@ -2,6 +2,65 @@
 
 dx3d::RenderSystem::RenderSystem() {
 
+	// We can combine multiple flags using the bitwise OR operator
+	// In this case we only want to add the flag that enables the
+	// D3D debug layer, which helps us get error and warning messages during runtime
+	UINT createDeviceFlags{};
+
+	// Where the feature level chosen internally by the function will be stored
+	D3D_FEATURE_LEVEL featureLevel{};
+
+#ifdef _DEBUG
+
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+
+#endif
+
+	// This fucntion allows us to create the D3D11 device for which we
+	// will be able to create all the graphics resources we need to render
+	// our window
+
+	// DXGI Adapter represents a GPU installed on the system, it can also represent
+	// other adapters, passing NULL as the first parameter and as the second, D3D will
+	// take the primary GPU.
+
+	// The next parameter expects a handle to a DLL that implements a custom software
+	// rasterizer. Since we are not using one, we can just pass null
+
+	// Then we can pass some flag values defined above
+
+	// The function requires a pointer to an array of feature level. By passing NULL D3D
+	// will simply pick the best feature level supported by the system.
+	// For now this behavior is good enough
+
+	// The next parameter is the size of the previous array, since we passed NULL we can
+	// safely pass 0
+
+	// Now that we handled those we need to pass the SDK version parameter
+	// This is a safety check used by Microsoft to prevent version mismatch and ensures our program
+	// links with the correct D3D runtime
+
+	// The first output parameter is the most important, the D3D device, it represents the virtual GPU
+	// and is used to create and manage all GPU related resources, like textures, shaders, buffers...
+
+	// To pass the COM pointer we use the & operator, this gives us a double pointer, which is what
+	// the function expects
+
+	// The next output parameter is the feature level chosen internally by the function
+
+	// Last we need to retrieve the device context
+	auto hr = D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, NULL, 0, D3D11_SDK_VERSION,
+		&m_d3dDevice, &featureLevel, &m_d3dContext);
+
+	// At this point we need to check the return function so we can throw an exception
+	// if something goes wrong
+
+	// In this case we can use an handy macro called FAILED
+	if (FAILED(hr)) {
+
+		throw std::runtime_error("Direct3D11 initialization failed");
+	}
+
 }
 
 dx3d::RenderSystem::~RenderSystem() {
--- DX3D/Source/DX3D/Graphics/RenderSystem.h
+++ DX3D/Source/DX3D/Graphics/RenderSystem.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <DX3D/Core/Core.h>
 #include <DX3D/Core/Base.h>
+#include <d3d11.h>
+#include <wrl.h>
 
 namespace dx3d {
 
@@ -10,5 +13,27 @@ namespace dx3d {
 
 		RenderSystem();
 		virtual ~RenderSystem() override;
+
+	private:
+
+		// Just like we did with the Window instance, we should use
+		// smart pointers. Unlike the Window class, however, we can
+		// choose a standard C++ smart pointer, like unique pointer
+
+		// The 3D resources, like 3D devices are COM objects, which
+		// use reference counting for memory management
+
+		// Reference counting is a technique where each object keeps
+		// track of how many references or owners point to it
+		// When a new reference is created the count goes up
+		// When a reference is released the count goes down
+		// When the count reaches zero, the object is automatically destroyed
+		// For these types of object we need to use COM pointer, a smart pointer
+		// provided by the Windows Runtime C++ Template Library, which we can access
+		// from the wrl.h header file
+
+		Microsoft::WRL::ComPtr<ID3D11Device> m_d3dDevice{};
+		Microsoft::WRL::ComPtr<ID3D11DeviceContext> m_d3dContext{};
 	};
+
 }
